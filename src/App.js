f5c65564965100b761b6e936f33908441576b3aa
Ali Safdar Kureishy <safdar.kureishy@gmail.com>
2020-11-06 20:38:47 -0800

    Initial

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Safdar <code>src/App.js</code> and save to WIN millions!
         </p>
         <a
           className="App-link"
